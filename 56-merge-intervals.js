f5d683a856c35ad81fd5d371e4decb1067339a09
Robin Kim <robincodes@pm.me>
2019-10-14 09:01:37 -0700

    Create 56-merge-intervals.js

---
+++ 56-merge-intervals.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/merge-intervals/
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+  if(intervals.length <= 1) {
+    return intervals;
+  }
+  intervals.sort((a, b) => {
+    return a[0] - b[0];
+  });
+  
+  var results = [];
+  
+  let currInterval = intervals[0].slice();
+  let nextInterval = intervals[1].slice();
+  for(var i = 1; i <= intervals.length; i++) {
+    if(nextInterval[0] <= currInterval[1]) {
+      currInterval[1] = Math.max(currInterval[1], nextInterval[1]);
+    }
+    if(currInterval[1] < nextInterval[0]) {
+      results.push(currInterval);
+      currInterval = nextInterval;
+    }
+    nextInterval = intervals[i+1] || [Infinity, Infinity];
+  }
+  
+  return results;
+};

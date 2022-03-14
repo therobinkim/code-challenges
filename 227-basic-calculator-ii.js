c14b2eb2274d2fe7b773692a85c6a71e20c6fa2e
Robin Kim <robincodes@pm.me>
2022-03-13 20:36:30 -0500

    Update 227-basic-calculator-ii.js

--- 227-basic-calculator-ii.js
+++ 227-basic-calculator-ii.js
@@ -1,5 +1,5 @@
 /**
- * @param {string} s
+ * @param {string} input
  * @return {number}
  */
 
@@ -15,61 +15,43 @@
 */
 var calculate = function(input) {
   if(!input.length) return 0;
-  var stack = [getNextNumber(input)];
-  // console.log(stack);
-  while(true) {
-    var nextOperatorPosition = indexOfOperator(input);
-    if(nextOperatorPosition === -1) {
-      break;
-    }
-    var operator = input[nextOperatorPosition];
-    // console.log(operator);
-    // console.log(getNextNumber(input))
-    input = input.slice(nextOperatorPosition + 1);
+  
+  var stack = [];
+  var rest = '+' + input.trim(); // sets first operator to '+'
+  
+  do {
+    var [operator, number, rest] = next(rest);
     if(operator === '+') {
-      stack.push(getNextNumber(input));
+      stack.push(number);
     } else if(operator === '-') {
-      stack.push(getNextNumber(input) * -1);
+      stack.push(number * -1);
     } else if (operator === '/') {
-      stack.push(divide(stack.pop(), getNextNumber(input)));
+      stack.push(Math.trunc(stack.pop() / number));
     } else if(operator === '*') {
-      stack.push(stack.pop() * getNextNumber(input));
+      stack.push(stack.pop() * number);
     } else {
       throw new Error('Unexpected operator: ' + operator);
     }
-  }
+  } while(rest.length);
 
   return stack.reduce(function sum(accumulator, number) {
     return accumulator + number;
   }, 0);
-  
-  
-  function divide(numerator, denominator) {
-    var count = 0;
-    var isNegative = numerator < 0;
-    if(isNegative) numerator *= -1;
-    while(denominator <= numerator) {
-      numerator -= denominator;
-      count++;
-    }
-    return isNegative ? count * -1 : count;
+
+  function next(input) {
+    if(!input.length) return [null, 0, ''];
+    var operator = input[0];
+    var nextOperatorPosition = indexOfNextOperator(input, 1);
+    var number = Number(input.slice(1, nextOperatorPosition).trim());
+    return [operator, number, input.slice(nextOperatorPosition)]
   }
-  
-  function getNextNumber(string) {
-    var operatorPosition = indexOfOperator(string);
-    if(operatorPosition === -1) {
-      return Number(string.trim());
-    } else {
-      return Number(string.substring(0, operatorPosition).trim());
-    }
-  }
-  
-  function indexOfOperator(string) {
-    for(var i = 0; i < string.length; i++) {
-      if(('-+*/').includes(string[i])) {
+
+  function indexOfNextOperator(input, start = 0) {
+    for(var i = start; i < input.length; i++) {
+      if(('-+*/').includes(input[i])) {
         return i;
       }
     }
-    return -1;
+    return Infinity;
   }
 };
